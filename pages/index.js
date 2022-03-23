99a618e52e8dd4f46fdc0003218e5541fdfcce91
Elvis Morales <elvis@parenthesis.io>
2022-03-22 19:33:11 -0700

    Display out available NFTs in the marketplace.

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
       CSMarket.abi,
       provider
     );
-    const data = await marketContract.fetchMarketItems();
+    const data = await marketContract.fetchMarketTokens();
 
     const items = await Promise.all(
       data.map(async (i) => {
@@ -76,5 +76,42 @@ export default function Home() {
     loadNFTs();
   }
 
-  return <div>CUBANSEA MARKETPLACE</div>;
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="px-20 py-7 text-4x1">No NFTs in marketplace</h1>;
+
+  return (
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "160px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-x1 overflow-hidden">
+              <img src={nft.image} />
+              <div className="p-4">
+                <p
+                  style={{ height: "64px" }}
+                  className="text-3x1 font-semibold"
+                >
+                  {nft.name}
+                </p>
+                <div style={{ height: "72px", overflow: "hidden" }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-3x-1 mb-4 font-bold text-white">
+                  {nft.price} ETH
+                </p>
+                <button
+                  className="w-full bg-purple-500 text-white font-bold py-3 px-12 rounded"
+                  onClick={() => buyNFT(nft)}
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
