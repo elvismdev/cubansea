bf0a57379817e0778f5ebee175908b66a8dbe692
Elvis Morales <elvis@parenthesis.io>
2022-03-22 18:40:01 -0700

    Function to load NFT's to the market.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,54 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
 
+import { nftaddress, nftmarketaddress } from "../config";
+
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import CSMarket from "../artifacts/contracts/CSMarket.sol/CSMarket.json";
+
 export default function Home() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    // What we want to load:
+    // *** provider, tokenContract, marketContract, data for our marketItems ***
+
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      CSMarket.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenUri(i.tokenId);
+        // We want to get the token metadata - json.
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+
   return <div>CUBANSEA MARKETPLACE</div>;
 }
