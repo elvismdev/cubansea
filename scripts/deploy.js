81a49a8de367e7234e52adbd7cd5eb6878112964
Elvis Morales <emorales@forumone.com>
2022-04-05 22:03:41 -0700

    Do not write Smart Contract addresses to the file system anymore. These are now manually set in environment variables and read from there.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const hre = require("hardhat");
-const fs = require("fs");
+// const fs = require("fs");
 
 async function main() {
   const NFTMarket = await hre.ethers.getContractFactory("CSMarket");
@@ -14,12 +14,12 @@ async function main() {
 
   console.log("NFT contract deployed to: ", nft.address);
 
-  let config = `
-  export const nftmarketaddress = "${nftMarket.address}";
-  export const nftaddress = "${nft.address}";`;
+  // let config = `
+  // export const nftmarketaddress = "${nftMarket.address}";
+  // export const nftaddress = "${nft.address}";`;
 
-  let data = JSON.stringify(config);
-  fs.writeFileSync("config.js", JSON.parse(data));
+  // let data = JSON.stringify(config);
+  // fs.writeFileSync("config.js", JSON.parse(data));
 }
 
 main()
