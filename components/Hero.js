3fa456337b3cb0e97afb6d691579b7e173b92197
Karen Kitchens <karen.kitchens@gmail.com>
2022-04-05 21:18:02 -0500

    make hero responsive

--- components/Hero.js
+++ components/Hero.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const style = {
   wrapper: `relative`,
-  container: `before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('https://i.ibb.co/ZHvvFBr/istockphoto-157434748.jpg')] before:bg-cover before:bg-center before:opacity-50 before:blur-[3px]`,
+  container: `max-w-7xl mx-auto before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('https://i.ibb.co/ZHvvFBr/istockphoto-157434748.jpg')] before:bg-cover before:bg-center before:opacity-50`,
   contentWrapper: `flex h-screen relative justify-center flex-wrap items-center`,
-  copyContainer: `w-1/2`,
-  title: `relative text-white text-[46px] font-semibold max-w-[550px]`,
-  description: `text-[#d9d9d9] container-[400px] text-2xl mt-[0.8rem] mb-[2.5rem] max-w-[500px]`,
-  ctaContainer: `flex`,
+  copyContainer: ``,
+  title: `relative text-white text-[46px] font-semibold md:max-w-[550px] text-center md:text-left`,
+  description: `text-[#d9d9d9] container-[400px] text-2xl mt-[0.8rem] mb-[2.5rem] md:max-w-[500px] text-center md:text-left`,
+  ctaContainer: `md:flex text-center`,
   accentedButton: ` relative text-lg font-semibold px-12 py-4 bg-[#2181e2] rounded-lg mr-5 text-white hover:bg-[#42a0ff] cursor-pointer`,
   button: ` relative text-lg font-semibold px-12 py-4 bg-[#363840] rounded-lg mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer`,
   cardContainer: `rounded-[3rem]`,
@@ -40,26 +40,28 @@ const Hero = () => {
               </Link>
             </div>
           </div>
-          <div className={style.cardContainer}>
-            <img
-              className="rounded-t-lg"
-              src="https://lh3.googleusercontent.com/LwSL45hmFmoSVu5C6HuWGuxt2kxcI_xw6HS9oq4YUhpp81qnJfSibsauZSdsDyCk48eQ01EPlx6jzCld9uWt-DkukLTa-KYw6l0GaAA=s550"
-              alt=""
-            />
-            <div className={style.infoContainer}>
+          <div className={style.rightContainer}>
+            <div className={style.cardContainer}>
               <img
-                className="h-[2.25rem] rounded-full"
-                src="https://avatars.githubusercontent.com/u/3847077?v=4"
+                className="rounded-t-lg"
+                src="https://lh3.googleusercontent.com/LwSL45hmFmoSVu5C6HuWGuxt2kxcI_xw6HS9oq4YUhpp81qnJfSibsauZSdsDyCk48eQ01EPlx6jzCld9uWt-DkukLTa-KYw6l0GaAA=s550"
                 alt=""
               />
-              <div className={style.author}>
-                <div className={style.name}>n3rdh4ck3r</div>
-                <a
-                  className="text-[#1868b7]"
-                  href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/8970768298436072921991073414855549470908393302335808206645720822200320131077"
-                >
-                  An Ugly Dance with the Sky
-                </a>
+              <div className={style.infoContainer}>
+                <img
+                  className="h-[2.25rem] rounded-full"
+                  src="https://avatars.githubusercontent.com/u/3847077?v=4"
+                  alt=""
+                />
+                <div className={style.author}>
+                  <div className={style.name}>n3rdh4ck3r</div>
+                  <a
+                    className="text-[#1868b7]"
+                    href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/8970768298436072921991073414855549470908393302335808206645720822200320131077"
+                  >
+                    An Ugly Dance with the Sky
+                  </a>
+                </div>
               </div>
             </div>
           </div>
